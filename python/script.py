e8673342dd66bb05528f65edd57a2b3cfd0065d8
Akshay Biradar <53450422+akshaybiradar-ab@users.noreply.github.com>
2025-08-06 14:29:55 +0530

    Update script.py

--- python/script.py
+++ python/script.py
@@ -9,11 +9,11 @@ greet("John")
 
 # Workflow Error
 Run flake8 python/
-python/script.py:20:1: 
+python/script.py:20:1:
 E302 expected 2 blank lines, found 1
-python/script.py:21:29: 
+python/script.py:21:29:
 E261 at least two spaces before inline comment
-python/script.py:22:1: 
+python/script.py:22:1:
 E305 expected 2 blank lines after class or function definition, found 0
 Error: Process completed with exit code 1.
 
@@ -26,6 +26,7 @@ def greet(name):
 greet("John")
 ------------------------------------------
 """
+
 
 def greet(name):
     print(f"Hello, {name}!")
